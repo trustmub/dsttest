d36499230f74da95f112be9411984fc9d384db78
trust  mubaiwa <trustmub@trusts-MacBook-Pro.local>
2019-02-20 21:01:58 +0200

    setting the CORS file

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,18 +1,43 @@
-import {Observable, Observer} from 'rxjs';
+import {EventEmitter, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import 'rxjs';
-import {EventEmitter} from '@angular/core';
 
+@Injectable()
 export class AuthenticationService {
 
   isAuthenticated = new EventEmitter<boolean>();
+  private baseUrl = 'https://etracking-dst.herokuapp.com/';
 
   // authenticationObserver: Observer<boolean>;
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
   }
 
   getAuthenticationStatus() {
+    this.loginUser();
     return this.isAuthenticated;
+  }
+
+  /**
+   * login object structure
+   *
+   * {
+   *    "email": "trustmub@gmail.com",
+   *    "password": "password"
+   * }
+   *
+   **/
+
+  loginUser() {
+    this.httpClient.get(this.baseUrl + 'api/user/login', {
+      observe: 'response',
+      // body: {email: 'trustmub@gmail.com', password: 'password'}
+    }).subscribe(
+      (response) => {
+        console.log(response);
+        return '';
+      }
+    );
   }
 
   setAuthentication(auth: boolean) {
